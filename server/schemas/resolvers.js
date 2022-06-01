153c91f7b735575939227ac609d76164aec216eb
Christi Lewis Cvijanovic <95652938+ChristiLewis@users.noreply.github.com>
2022-06-01 10:29:51 -0700

    resolvers mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -9,21 +9,21 @@ const resolvers = {
             const userData = await User.findOne({})
                 .select('-__v -password')
                 .populate('books');
-            // .populate('friends');
 
             return userData;
         },
-  
-        },
+
         //ALL
         books: async (parent, { username }) => {
             const params = username ? { username } : {};
             return Book.find(params).sort({ createdAt: -1 });
         },
+
         //ONE BY ID
         book: async (parent, { _id }) => {
             return Book.findOne({ _id });
         },
+
         //ALL USERS
         users: async () => {
             return User.find()
@@ -31,15 +31,15 @@ const resolvers = {
                 .populate('friends')
                 .populate('books');
         },
+
         //USER BY USERNAME
         user: async (parent, { username }) => {
             return User.findOne({ username })
                 .select('-__v -password')
                 .populate('friends')
                 .populate('books');
-        },
+        }
     },
-
 
     Mutation: {
         addUser: async (parent, args) => {
@@ -48,6 +48,7 @@ const resolvers = {
 
             return { token, user };
         },
+
         login: async (parent, { email, password }) => {
             const user = await User.findOne({ email });
 
@@ -63,12 +64,29 @@ const resolvers = {
 
             const token = signToken(user);
             return { token, user };
+        },
+
+        addBook: async (parent, args, context) => {
+            if (context.user) {
+                const thought = await Book.create({ ...args, username: context.user.username });
+
+                await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $push: { books: book._id } },
+                    { new: true }
+                );
+
+                return book;
+            }
+
+            throw new AuthenticationError('You need to be logged in!');
         }
+
     }
 
 };
 
 module.exports = resolvers;
-    
+
 
 
